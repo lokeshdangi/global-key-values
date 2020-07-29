a2182ce2bcc50319cb706f62bc92793680b40e2b
Lokesh Dangi <lokeshdangi002@gmail.com>
2020-07-29 16:24:49 +0530

    multiple get and set added

---
+++ index.js
@@ -0,0 +1,35 @@
+let store = {};
+
+export const set = (key, value) => {
+  store[key] = value;
+};
+
+export const setMultiple = keyValuePair => {
+  keyValuePair.map(o => {
+    store[o.key] = o.value;
+  });
+};
+
+export const get = key => {
+  return store[key];
+};
+
+export const getMultiple = keys => {
+  keys.reduce((acc, o) => {
+    acc[o] = store[o];
+    return acc;
+  }, {});
+};
+
+export const reset = () => {
+  store = {};
+};
+
+export const GlobalStore = {
+  set,
+  get,
+  getMultiple,
+  reset
+};
+
+export default store;
